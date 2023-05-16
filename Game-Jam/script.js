d7dccbc78c258ca9075f0ca698a2e85242a88a5f
jernejmarcic <91026972+jernejmarcic@users.noreply.github.com>
2023-05-16 14:11:09 +0200

    made it way better

--- Game-Jam/script.js
+++ Game-Jam/script.js
@@ -20,20 +20,26 @@ function switchPage(pageId) {
 function toggleDarkMode() {
     darkMode = !darkMode;
     if (darkMode) {
-        document.documentElement.style.setProperty('--background-color', 'rgba(0, 0, 0, 1)');
-        document.documentElement.style.setProperty('--text-color', 'rgba(255, 255, 255, 1)');
-        document.documentElement.style.setProperty('--nav-background-color', 'rgba(255, 255, 255, 1)');
+        document.documentElement.style.setProperty('--background-color', 'rgba(17, 17, 27, 1)');
+        document.documentElement.style.setProperty('--text-color', 'rgba(205, 214, 244, 1)');
+        document.documentElement.style.setProperty('--h1-color', 'rgba(243, 139, 168, 1)');
+        document.documentElement.style.setProperty('--nav-background-color', 'rgba(180, 190, 254, 1)');
         document.documentElement.style.setProperty('--nav-text-color', 'rgba(0, 0, 0, 1)');
         document.documentElement.style.setProperty('--nav-hover-background-color', 'rgba(0, 0, 0, 1)');
         document.documentElement.style.setProperty('--nav-hover-text-color', 'rgba(255, 255, 255, 1)');
     } else {
         document.documentElement.style.setProperty('--background-color', 'rgba(255, 255, 255, 1)');
         document.documentElement.style.setProperty('--text-color', 'rgba(0, 0, 0, 1)');
-        document.documentElement.style.setProperty('--nav-background-color', 'rgba(51, 51, 51, 1)');
-        document.documentElement.style.setProperty('--nav-text-color', 'rgba(255, 255, 255, 1)');
+        document.documentElement.style.setProperty('--h1-color', 'rgba(243, 139, 168, 1)');
+        document.documentElement.style.setProperty('--nav-background-color', 'rgba(49, 50, 68, 1)');
+        document.documentElement.style.setProperty('--nav-text-color', 'rgba(205, 214, 244, 1)');
         document.documentElement.style.setProperty('--nav-hover-background-color', 'rgba(221, 221, 221, 1)');
         document.documentElement.style.setProperty('--nav-hover-text-color', 'rgba(0, 0, 0, 1)');
     }
 }
 
 switchPage('home');
+
+document.getElementById("paragraphWidth").oninput = function() {
+    document.documentElement.style.setProperty('--p-width', this.value + 'px');
+}
