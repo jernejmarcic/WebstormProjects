ba99e59ae4bb2fc6a320c64ff30b87341fae3fd5
jernejmarcic <91026972+jernejmarcic@users.noreply.github.com>
2023-05-22 21:02:02 +0200

    JavaScript makes me angery
    Added leaves that are procedually generated and displayed over the screen, also added a progress bar to see how far down you have scrolled

--- Game-Jam/script.js
+++ Game-Jam/script.js
@@ -45,3 +45,48 @@ document.getElementById("paragraphWidth").oninput = function() {
     document.documentElement.style.setProperty('--p-margin', (window.innerWidth - this.value) / 2 + 'px');
 }
 
+var leafLimit = 100;
+var leafCount = 0;
+var initialInterval = 500;
+var maxInterval = 5000;
+var interval = initialInterval;
+
+function addLeaf() {
+    if (leafCount < leafLimit) {
+        var newLeaf = document.createElement('div');
+        newLeaf.classList.add('leaf');
+
+        // Apply random size and color to each leaf
+        if (Math.random() > 0.5) newLeaf.classList.add('big');
+        if (Math.random() > 0.5) newLeaf.classList.add('green');
+        else newLeaf.classList.add('yellow');
+
+        // Append to container to calculate offset width
+        var container = document.getElementById('jungle-container');
+        container.appendChild(newLeaf);
+
+        // Randomly position the leaf on the sides of the screen
+        var side = Math.random() > 0.5 ? 0 : window.innerWidth;
+        newLeaf.style.left = side - newLeaf.offsetWidth + 'px';
+        newLeaf.style.top = Math.random() * window.innerHeight + 'px';
+
+        leafCount++;
+
+        // Gradually increase interval, capped at maxInterval
+        if (interval < maxInterval) {
+            interval *= 1.05;
+        }
+
+        setTimeout(addLeaf, interval);
+    }
+}
+
+setTimeout(addLeaf, interval);
+
+
+window.onscroll = function() {
+    var bodyHeight = document.body.scrollHeight - innerHeight;
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var percent = scrollTop / bodyHeight * 100;
+    document.getElementById('progress-bar').style.width = percent + '%';
+}
